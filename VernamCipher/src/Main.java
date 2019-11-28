f547d345abbbf904eb1fae6f3830c0986b2cbd1e
RePock <mikov08.08@gmail.com>
2019-11-28 18:57:25 +0700

    VernamCipher

--- VernamCipher/src/Main.java
+++ VernamCipher/src/Main.java
@@ -1,10 +1,7 @@
-import java.awt.event.KeyEvent;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Random;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
